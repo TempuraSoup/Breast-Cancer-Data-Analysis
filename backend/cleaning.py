cc0cceb72b8f6de831f6a229ee1780368f8fea78
Crolwick <UIGiren@gmail.com>
2023-09-04 16:30:29 -0700

    dictionary for values

--- backend/cleaning.py
+++ backend/cleaning.py
@@ -6,9 +6,36 @@
     flask
     flask-cors
 '''
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
 
-datafile = open("backend/assets/data/breast-cancer.data", "r")
+df = pd.read_csv('backend/assets/data/breast-cancer.data', header=None, names=["Class", "Age", "Menopause", "TumorSize", "Inv-Nodes", "Node-Caps", "Deg-Malig", "Breast", "breast-Quad", "Irrdiat"])
 
-print(datafile.read())
+age = df.loc[:, "Age"].sort_values()
 
-datafile.close()
+tumorSize = df.loc[:, "TumorSize"]
+
+tumorSizeLabels = tumorSize.sort_values().unique()
+
+print(tumorSizeLabels)
+
+dict = {'30-34': 0, '20-24': 0}
+
+print(dict)
+
+for tumor in tumorSize:
+    print(tumor)
+    dict[tumor] += 1
+
+print(dict)
+
+
+'''
+plt.bar(age, tumorSize, color="red")
+
+plt.xlabel("Age")
+plt.ylabel("Tumor Size")
+plt.title("Age vs Tumor Size")
+plt.show()
+'''
